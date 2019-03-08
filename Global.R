786717eb85549566f5ed4b7e8154aa33c5300426
ThiagoValentimMarques <48340105+ThiagoValentimMarques@users.noreply.github.com>
2019-03-07 23:15:24 -0300

    Create Global.R

---
+++ Global.R
@@ -0,0 +1,15 @@
+# Used packages
+pacotes = c("shiny", "shinydashboard", "shinythemes", "plotly", "shinycssloaders","tidyverse",
+            "scales", "knitr", "kableExtra", "ggfortify","dplyr","plotly","FNN")
+
+# Run the following command to verify that the required packages are installed. If some package
+# is missing, it will be installed automatically
+package.check <- lapply(pacotes, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
+  }
+})
+
+# Define working directory
+data <- read.table("PES2019.txt",header=TRUE,sep="\t")
+names(data)[1] <- "player"  #rename from "ï..player" to "player"
